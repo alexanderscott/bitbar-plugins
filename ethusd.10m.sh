4dd597bd72381d9b2442fdeb06b63b611ef94c67
Alex Ehrnschwender <alexanderscott46@gmail.com>
2017-05-04 14:36:35 -0700

    add eth & stock plugins

---
+++ ethusd.10m.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Shows last Ethereum price in USD.
+#
+# <bitbar.title>Ethereum last price</bitbar.title>
+# <bitbar.version>0.1B</bitbar.version>
+# <bitbar.author>Nikita Zhavoronkov</bitbar.author>
+# <bitbar.author.github>Har01d</bitbar.author.github>
+# <bitbar.desc>Shows last Ethereum price in USD.</bitbar.desc>
+# <bitbar.image>http://i.imgur.com/lF2AA7o.png</bitbar.image>
+#
+# by Nikita Zhavoronkov
+# Based on Coinbase bitbar plugin by Mat Ryer
+
+echo -n "𝚵"; curl -s "https://www.cryptonator.com/api/ticker/eth-usd" | egrep -o '"price":"[0-9]+(\.)?([0-9]{1})"?' | sed 's/"price":/ /' | sed 's/\"//g'
